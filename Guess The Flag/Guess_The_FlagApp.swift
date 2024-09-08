8c78ef3d58cae037b0e8b55435d826911acb5469
Christopher Carlton <christopher.brett.carlton@gmail.com>
2024-09-08 14:37:49 -0400

    Initial Commit

---
+++ Guess The Flag/Guess_The_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_The_FlagApp.swift
+//  Guess The Flag
+//
+//  Created by Christopher Carlton on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Guess_The_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
